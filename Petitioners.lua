333d3dc345cc46785d5ac28823b676ae69462ba5
Stikyhooves <111915077+Stikyhooves@users.noreply.github.com>
2025-03-03 14:16:15 -0500

    Update Petitioners.lua - Accomodated Groups
    Script now accounts for groups. Each group member will appear on the list.

--- Petitioners.lua
+++ Petitioners.lua
@@ -17,7 +17,7 @@ VistorsWindow.ATTRS {
 
 function VistorsWindow:init()
     self:addviews{
-	    widgets.WrappedLabel{  -- Holding this space in case 
+        widgets.WrappedLabel{  -- Holding this space in case 
             frame={l=0, r=0, t=0},
             text_to_wrap='Click on a petitioner to center on them for futher inspection.', 
         },
@@ -33,39 +33,59 @@ function VistorsWindow:init()
 end
 
 function VistorsWindow:initListChoices()
- 
--- Checks for current peitions/petitioners 
--- Not tested for troupes
 
         local choices = {}
         for _,p in pairs (df.global.plotinfo.petitions) do  -- for each petition on screen
         local petition_id = p
             for _,agmt in pairs(df.global.world.agreements.all) do --- Get agreement full list
                 local agreement_id = agmt.id  -- compare agreement id to petition id
-                if agreement_id == petition_id then                     
-                    party0_unit = df.global.world.agreements.all[agreement_id].parties[0].histfig_ids[0]
-                
-                    for _,u in pairs(df.global.world.units.active) do -- now check those hfigs against unit ids to get info on them (may be improved)
-						local unit_hxfig_id = u.hist_figure_id
-						if unit_hxfig_id == party0_unit then 
-							--print(unit_hxfig_id, " - " , party0_unit)
-							local u_name = dfhack.translation.translateName(u.name)
-							local trans_name = dfhack.translation.translateName(u.name, true, true)
-							local u_caste = get_caste_name(u.race, u.caste, u.profession)
-							local u_race = dfhack.units.getRaceName(u)
-							--print(u_race)
-							if df.global.world.agreements.all[agmt.id].details[0].type == 2 then 
-								text = "Residency: "..u_name .. " (" .. trans_name.. ") - " .. u_caste .. " (" .. u_race .. ")"
-								--table.insert(choices, {text=text, data={unit=u, group=groupIndex}})
-								table.insert(choices, {text=text, data={unit=u, group=0}})
-							
-							elseif df.global.world.agreements.all[agmt.id].details[0].type == 3 then     
-								text = "Citizenship: ".. u_name .. " (" .. trans_name.. ") - " .. u_caste        
-								--table.insert(choices, {text=text, data={unit=u, group=groupIndex}})
-								table.insert(choices, {text=text, data={unit=u, group=0}})
-								
-							end
-						end
+                if agreement_id == petition_id then      
+                    if #df.global.world.agreements.all[agreement_id].parties[0].histfig_ids ~=0 then  -- filters out cases where the hisfig is empty - such as with troupes
+                    
+                        party0_unit = df.global.world.agreements.all[agreement_id].parties[0].histfig_ids[0]
+                    
+                        for _,u in pairs(df.global.world.units.active) do -- now check those hfigs against unit ids to get info on them (may be improved)
+                            local unit_hxfig_id = u.hist_figure_id
+                            if unit_hxfig_id == party0_unit then 
+                                --print(unit_hxfig_id, " - " , party0_unit)
+                                local u_name = dfhack.translation.translateName(u.name)
+                                local trans_name = dfhack.translation.translateName(u.name, true, true)
+                                local u_caste = get_caste_name(u.race, u.caste, u.profession)
+                                local u_race = dfhack.units.getRaceName(u)
+                                --print(u_race)
+                                if df.global.world.agreements.all[agmt.id].details[0].type == 2 then 
+                                    text = "Residency: "..u_name .. " (" .. trans_name.. ") - " .. u_caste .. " (" .. u_race .. ")"
+                                    --table.insert(choices, {text=text, data={unit=u, group=groupIndex}})
+                                    table.insert(choices, {text=text, data={unit=u, group=0}})
+                                
+                                elseif df.global.world.agreements.all[agmt.id].details[0].type == 3 then     
+                                    text = "Citizenship: ".. u_name .. " (" .. trans_name.. ") - " .. u_caste .. " (" .. u_race .. ")"        
+                                    --table.insert(choices, {text=text, data={unit=u, group=groupIndex}})
+                                    table.insert(choices, {text=text, data={unit=u, group=0}})
+                                end    
+                            end
+                        end
+                    else -- assumes petition is from a group since it was not from an individual
+                        local group_id = df.global.world.agreements.all[agreement_id].parties[0].entity_ids[0]
+                        local group_name = dfhack.translation.translateName(df.global.world.entities.all[group_id].name, true, true)
+                        
+                        for _,ent in pairs(df.global.world.entities.all[group_id].hist_figures) do -- for each unit in the group
+
+                            for _,u in pairs(df.global.world.units.active) do  -- check unit against active units (used to get unit hex from histfig)
+                                local unit_hxfig_id = u.hist_figure_id
+                                if unit_hxfig_id == ent.id then 
+                                
+                                    local u_name = dfhack.translation.translateName(u.name)
+                                    local trans_name = dfhack.translation.translateName(u.name, true, true)
+                                    local u_caste = get_caste_name(u.race, u.caste, u.profession)
+                                    local u_race = dfhack.units.getRaceName(u)
+                                                
+                                    text = group_name..": ".. u_name .. " (" .. trans_name.. ") - " .. u_caste .. " (" .. u_race .. ")"    
+                                    
+                                    table.insert(choices, {text=text, data={unit=u, group=0}})
+                                end
+                            end
+                        end    
                     end
                 end
             end
@@ -76,7 +96,6 @@ end
 function VistorsWindow:onZoom()
     local _, choice = self.subviews.list:getSelected()
     local unit = choice.data.unit
-
     local target = xyz2pos(dfhack.units.getPosition(unit))
     dfhack.gui.revealInDwarfmodeMap(target, true, true)
 end
