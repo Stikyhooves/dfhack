1ed01c61d13e918ae18d955eca1c55606537bb78
Stikyhooves <111915077+Stikyhooves@users.noreply.github.com>
2025-03-03 17:47:34 -0500

    Update Petitioners.lua - Changed frame position, added struture for detail text

--- Petitioners.lua
+++ Petitioners.lua
@@ -7,13 +7,13 @@ function get_caste_name(race, caste, profession)
     return dfhack.units.getCasteProfessionName(race, caste, profession)
 end
 
-VistorsWindow = defclass(VistorsWindow, widgets.Window)
-VistorsWindow.ATTRS {
-    frame_title='Current Petitioners',
-    frame={w=80, h=31},
-    resizable=true,
-    resize_min={w=50, h=20},
-}
+
+function get_hfighex(hf_id) --keep
+--- Retrieves the hex value of a histfig. 
+    return utils.binsearch(df.global.world.history.figures, hf_id, 'id')
+end
+
+
 
 function VistorsWindow:init()
     self:addviews{
@@ -25,8 +25,23 @@ function VistorsWindow:init()
             frame={l=0, r=0, t=3, b=6},
             view_id = 'list',
             on_select=self:callback('onZoom'),
-           -- on_double_click=self:callback('onIgnore'),
-           -- on_double_click2=self:callback('onToggleGroup'),
+        },
+        widgets.Panel{
+            view_id='footer',
+            frame={l=0, r=0, b=0, h=7},
+            frame_style=gui.FRAME_INTERIOR,
+            subviews={
+                widgets.WrappedLabel{
+                    frame={l=0, h=6},
+                    view_id='desc',
+                    auto_height=false,
+                    text_to_wrap=function()
+                        local list = self.subviews.list
+                        local _, choice = list:getSelected()
+                        return choice and choice.text_long or ''
+                    end,
+                },
+            },
         },
     }
     self:initListChoices()
@@ -45,23 +60,31 @@ function VistorsWindow:initListChoices()
                         party0_unit = df.global.world.agreements.all[agreement_id].parties[0].histfig_ids[0]
                     
                         for _,u in pairs(df.global.world.units.active) do -- now check those hfigs against unit ids to get info on them (may be improved)
-                            local unit_hxfig_id = u.hist_figure_id
-                            if unit_hxfig_id == party0_unit then 
-                                --print(unit_hxfig_id, " - " , party0_unit)
+                            local petitioner_hxfig_id = u.hist_figure_id
+                            if petitioner_hxfig_id == party0_unit then 
+ 
+                                
+                                local petitioner_hxfig_hex = get_hfighex(u.id)
+                                
                                 local u_name = dfhack.translation.translateName(u.name)
                                 local trans_name = dfhack.translation.translateName(u.name, true, true)
                                 local u_caste = get_caste_name(u.race, u.caste, u.profession)
                                 local u_race = dfhack.units.getRaceName(u)
+                                local petitioner_goal = dfhack.units.getGoalName(u,0)
+                                
+                                
                                 --print(u_race)
                                 if df.global.world.agreements.all[agmt.id].details[0].type == 2 then 
                                     text = "Residency: "..u_name .. " (" .. trans_name.. ") - " .. u_caste .. " (" .. u_race .. ")"
+                                    text_long = "Residency: "..u_name .. " (" .. trans_name.. ") - " .. u_caste .. " (" .. u_race .. ") \nGoal: " .. petitioner_goal 
                                     --table.insert(choices, {text=text, data={unit=u, group=groupIndex}})
-                                    table.insert(choices, {text=text, data={unit=u, group=0}})
+                                    table.insert(choices, {text=text, text_long=text_long, data={unit=u, group=0}})
                                 
                                 elseif df.global.world.agreements.all[agmt.id].details[0].type == 3 then     
-                                    text = "Citizenship: ".. u_name .. " (" .. trans_name.. ") - " .. u_caste .. " (" .. u_race .. ")"        
+                                    text = "Citizenship: ".. u_name .. " (" .. trans_name.. ") - " .. u_caste .. " (" .. u_race .. ")"    
+                                    text_long = "Citizenship: ".. u_name .. " (" .. trans_name.. ") - " .. u_caste .. " (" .. u_race .. ") \nGoal: " .. petitioner_goal    
                                     --table.insert(choices, {text=text, data={unit=u, group=groupIndex}})
-                                    table.insert(choices, {text=text, data={unit=u, group=0}})
+                                    table.insert(choices, {text=text, text_long=text_long, data={unit=u, group=0}})
                                 end    
                             end
                         end
@@ -72,17 +95,20 @@ function VistorsWindow:initListChoices()
                         for _,ent in pairs(df.global.world.entities.all[group_id].hist_figures) do -- for each unit in the group
 
                             for _,u in pairs(df.global.world.units.active) do  -- check unit against active units (used to get unit hex from histfig)
-                                local unit_hxfig_id = u.hist_figure_id
-                                if unit_hxfig_id == ent.id then 
+                                local petitioner_hxfig_id = u.hist_figure_id
+                                if petitioner_hxfig_id == ent.id then 
                                 
                                     local u_name = dfhack.translation.translateName(u.name)
                                     local trans_name = dfhack.translation.translateName(u.name, true, true)
                                     local u_caste = get_caste_name(u.race, u.caste, u.profession)
                                     local u_race = dfhack.units.getRaceName(u)
-                                                
-                                    text = group_name..": ".. u_name .. " (" .. trans_name.. ") - " .. u_caste .. " (" .. u_race .. ")"    
+                                    local petitioner_goal = dfhack.units.getGoalName(u,0)         
                                     
-                                    table.insert(choices, {text=text, data={unit=u, group=0}})
+                                    text = group_name..": ".. u_name .. " (" .. trans_name.. ") - " .. u_caste .. " (" .. u_race .. ")"  
+                                    
+                                    text_long = group_name..": ".. u_name .. " (" .. trans_name.. ") - " .. u_caste .. " (" .. u_race .. ") \nGoal: " .. petitioner_goal    
+                                    
+                                    table.insert(choices, {text=text, text_long=text_long, data={unit=u, group=0}})
                                 end
                             end
                         end    
@@ -97,8 +123,21 @@ function VistorsWindow:onZoom()
     local _, choice = self.subviews.list:getSelected()
     local unit = choice.data.unit
     local target = xyz2pos(dfhack.units.getPosition(unit))
-    dfhack.gui.revealInDwarfmodeMap(target, true, true)
+    dfhack.gui.revealInDwarfmodeMap(target, true, true) --- draw focus to selected unit
+    
+    local desc = self.subviews.desc
+    if desc.frame_body then desc:updateLayout() end --- update view with new focus text 
 end
+
+
+VistorsWindow = defclass(VistorsWindow, widgets.Window)
+VistorsWindow.ATTRS {
+    frame_title='Current Petitioners',
+    frame={w=80, h=31, r=3, b=5},
+    resizable=true,
+    resize_min={w=50, h=20},
+    
+}
 
 VistorsScreen = defclass(VistorsScreen, gui.ZScreen)
 VistorsScreen.ATTRS {
